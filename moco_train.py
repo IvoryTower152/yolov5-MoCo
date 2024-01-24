ebc045dea3560b338505d883d7cfd977cad423ec
IvoryTower152 <17749125422@163.com>
2024-01-24 20:41:08 +0800

    update proj

--- moco_train.py
+++ moco_train.py
@@ -24,6 +24,7 @@ import torchvision.transforms as transforms
 
 from utils.torch_utils import select_device
 from models.mocoBuilder import Model
+from moco.utils import AverageMeter, ProgressMeter, adjust_learning_rate, accuracy
 
 
 def get_parser():
@@ -74,7 +75,23 @@ def main():
 
 
 def step_one_train(train_loader, model, criterion, optimizer, epoch, args):
-    pass
+    batch_time = AverageMeter("Time", ":6.3f")
+    data_time = AverageMeter("Data", ":6.3f")
+    losses = AverageMeter("Loss", ":.4e")
+    top1 = AverageMeter("Acc@1", ":6.2f")
+    top5 = AverageMeter("Acc@5", ":6.2f")
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch),
+    )
+
+    model.train()
+
+    end = time.time()
+
+    for i, (images, _) in enumerate(train_loader):
+        pass
 
 
 if __name__ == "__main__":
